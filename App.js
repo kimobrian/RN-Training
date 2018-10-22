5e02e44a153980c874233ecaa1becb61c1f725a3
kimobrian <brian.kimokoti@andela.com>
2018-10-22 22:45:37 +0300

    chore: RN navigations

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+import AppNavigator from './routes';
+
+export default function App() {
+  return <AppNavigator />;
+}
+
+
+
+
---
+++ routes.js
@@ -0,0 +1,92 @@
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Platform } from 'react-native'
+
+import Contacts from './screens/Contacts';
+import Profile from './screens/Profile';
+import Favorites from './screens/Favorites';
+import User from './screens/User';
+
+import colors from './utils/colors';
+
+const getTabBarIcon = icon => ({ tintColor }) => (
+  <Icon name={icon} size={26} style={{ color: tintColor }} />
+);
+
+const ContactsScreens = createStackNavigator(
+  {
+    Contacts: {
+      screen: Contacts,
+    },
+    Profile: {
+      screen: Profile,
+    },
+  },
+  {
+    initialRouteName: 'Contacts'
+  },
+);
+
+ContactsScreens.navigationOptions = {
+  tabBarIcon: getTabBarIcon('list'),
+}
+
+const FavoritesScreens = createStackNavigator(
+  {
+    Favorites: {
+      screen: Favorites,
+    },
+    Profile: {
+      screen: Profile,
+    },
+  },
+  {
+    initialRouteName: 'Favorites'
+  },
+);
+
+FavoritesScreens.navigationOptions = {
+  tabBarIcon: getTabBarIcon('star'),
+}
+
+const UserScreens = createStackNavigator(
+  {
+    User: {
+      screen: User,
+    },
+  },
+  {
+    initialRouteName: 'User'
+  },
+);
+
+UserScreens.navigationOptions = {
+  tabBarIcon: getTabBarIcon('person'),
+}
+
+export default createBottomTabNavigator(
+  {
+    Contacts: {
+      screen: ContactsScreens,
+    },
+    Favorites: {
+      screen: FavoritesScreens,
+    },
+    User: {
+      screen: UserScreens,
+    },
+  },
+  {
+    initialRouteName: 'Contacts',
+    tabBarOptions: {
+      style: {
+        backgroundColor: colors.greyLight,
+      },
+      showLabel: true,
+      showIcon: true,
+      activeTintColor: colors.blue,
+      inactiveTintColor: colors.greyDark,
+    },
+  },
+);
