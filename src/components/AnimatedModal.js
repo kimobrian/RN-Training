c9cc61b4856409542158a7cdcd6701ecfee55f3e
kimobrian <brian.kimokoti@andela.com>
2018-10-24 14:48:54 +0300

    feat: Spring animation

--- src/components/AnimatedModal.js
+++ src/components/AnimatedModal.js
@@ -1,23 +1,54 @@
 import React, { Component } from "react";
-import { View, Text, Dimensions, TouchableOpacity } from "react-native";
+import { View, Text, Dimensions, TouchableOpacity, Animated, Easing } from "react-native";
 
 import Header from "./Header";
 const { height, width } = Dimensions.get("window");
 
 export default class AnimatedModal extends Component {
+  constructor(props) {
+    super(props);
+    this.yTranslate = new Animated.Value(0); // declare animated value for controlling the vertical position of the modal
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.visible) {
+      // animate the showing of the modal
+      this.yTranslate.setValue(0); // reset the animated value
+      Animated.spring(this.yTranslate, {
+        toValue: 1,
+        friction: 6 
+      }).start();
+    } else {
+      // animate the hiding of the modal
+      Animated.timing(this.yTranslate, {
+        toValue: 0,
+        duration: 200,
+        easing: Easing.linear
+      }).start();
+    }
+  }
+
   render() {
     const { title, image, children, onClose } = this.props;
     const bottomStyle = this.props.visible ? { bottom: 0 } : { bottom: -height };
 
+    let negativeHeight = -height + 20;
+    let modalMoveY = this.yTranslate.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, negativeHeight]
+    });
+
+    let translateStyle = { transform: [{ translateY: modalMoveY }] }; // translateY is the transform for moving objects vertically
+
     return (
-      <View style={[styles.container, bottomStyle]}>
+      <Animated.View style={[styles.container, translateStyle]}>
         <Header title={title}>
           <TouchableOpacity onPress={onClose} style={styles.button}>
             <Text style={styles.closeText}>Close</Text>
           </TouchableOpacity>
         </Header>
         <View style={styles.modalContent}>{children}</View>
-      </View>
+      </Animated.View>
     );
   }
 }
