54f3735641759ddad383050fa19b6be417c62adf
kimobrian <brian.kimokoti@andela.com>
2018-11-28 14:25:29 +0300

    feat: Include vector-icons

--- Authentication/Home.js
+++ Authentication/Home.js
@@ -1,53 +1,45 @@
-import React from 'react';
-import { Text, Button, View } from 'react-native';
-import styled from 'styled-components/native';
-import { GoogleSignin } from 'react-native-google-signin';
-
+import React, { Component } from "react";
+import { Text, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { GoogleSignin } from "react-native-google-signin";
 
 const StyledView = styled.View`
-  flex: 1; 
-  align-items: center; 
+  flex: 1;
+  align-items: center;
   justify-content: center;
-`
-
+`;
 
 const StyledViewB = styled.View`
   justify-content: center;
   align-items: center;
-  background-color: #F5FCFF;
-`
+  background-color: #f5fcff;
+`;
 
-const StyledText = styled.Text`
-  color: palevioletred;
-`
-
-export default class HomeScreen extends React.Component {
-
+export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userInfo: null,
-      error: null,
+      error: null
     };
   }
 
   async componentDidMount() {
     const isSignedIn = await GoogleSignin.isSignedIn();
-    if(!isSignedIn) navigate('Login');
+    if (!isSignedIn) navigate("Login");
     else {
       const { navigation } = this.props;
-      const userInfo = navigation.getParam('userInfo');
-      this.setState({ userInfo, error: null})
+      const userInfo = navigation.getParam("userInfo");
+      this.setState({ userInfo, error: null });
     }
   }
-
 
   renderError() {
     const { error } = this.state;
     if (!error) {
       return null;
     }
-    const text = `${error.toString()} ${error.code ? error.code : ''}`;
+    const text = `${error.toString()} ${error.code ? error.code : ""}`;
     return <Text>{text}</Text>;
   }
 
@@ -58,10 +50,10 @@ export default class HomeScreen extends React.Component {
       await GoogleSignin.signOut();
 
       this.setState({ userInfo: null, error: null });
-      navigate('Login');
+      navigate("Login");
     } catch (error) {
       this.setState({
-        error,
+        error
       });
     }
   };
@@ -69,12 +61,10 @@ export default class HomeScreen extends React.Component {
   renderUserInfo(userInfo) {
     return (
       <StyledViewB>
-        <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 20 }}>
+        <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 20 }}>
           Welcome {userInfo.user.name}
         </Text>
-        <Text>Your user info: {JSON.stringify(userInfo.user)}</Text>
-
-        <Button onPress={this._signOut} title="Log out" />
+        <Text>Your user info: {JSON.stringify(userInfo.user, null, 2)}</Text>
         {this.renderError()}
       </StyledViewB>
     );
@@ -82,12 +72,7 @@ export default class HomeScreen extends React.Component {
 
   render() {
     const { navigation } = this.props;
-    const userInfo = navigation.getParam('userInfo');
-    return (
-      <StyledView>
-        <Text>Home Screen</Text>
-        {this.renderUserInfo(userInfo)}
-      </StyledView>
-    );
+    const userInfo = navigation.getParam("userInfo");
+    return <StyledView>{this.renderUserInfo(userInfo)}</StyledView>;
   }
 }
--- Authentication/router.js
+++ Authentication/router.js
@@ -1,6 +1,15 @@
 import { createStackNavigator } from "react-navigation";
+import React from "react";
 import Login from "./App";
 import Home from "./Home";
+import Icon from "react-native-vector-icons/FontAwesome";
+import styled from "styled-components/native";
+
+
+const StyledButton = styled.TouchableOpacity`
+  align-items: center;
+  padding: 10px;
+`;
 
 export default createStackNavigator(
   {
@@ -12,9 +21,14 @@ export default createStackNavigator(
     },
     Home: {
       screen: Home,
-      navigationOptions: () => ({
-        title: "Home",
-        headerLeft: null
+      navigationOptions: ({ navigation: { getParam }}) => ({
+        title: `Welcome ${getParam('userInfo').user.givenName}`,
+        headerLeft: null,
+        headerRight: (
+          <StyledButton onPress={this._signOut}>
+            <Icon name="power-off" size={24} color="#900" />
+          </StyledButton>
+        )
       })
     }
   },
